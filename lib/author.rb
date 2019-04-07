e5e413dab56ec8366cfac713be689c1fd6c3bcde
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 22:37:31 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,7 +7,7 @@ class Author
   end
   
   def add_post(post_instance)
-  #  post_instance.author = self
-  #  @posts << post_instance
+    post_instance.author = self
+    @posts << post_instance
   end
 end
