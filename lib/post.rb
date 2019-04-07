2ce004a112521c96444ba35c1fafa8d1e6a0cf60
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 22:40:08 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,13 @@
 class Post
   attr_accessor :title, :author
+  @@all = []
   
   def initialize(title)
     @title = title
-    
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
